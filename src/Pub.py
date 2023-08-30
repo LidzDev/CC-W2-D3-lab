b59cc1ebed3054b6b428c632a9616fd524037522
LidzDev <lydia.cordell@gmail.com>
2023-08-30 16:47:56 +0100

    age check added

--- src/Pub.py
+++ src/Pub.py
@@ -4,8 +4,11 @@ class Pub:
         self.till = input_till
         self.drinks = []
 
-    def sell_drink(self, input_drink):
-        self.increase_till(input_drink.price)
+    def sell_drink(self, input_drink, input_customer_age):
+        if input_customer_age >= 21:
+            self.increase_till(input_drink.price)
+        else:
+            False
 
     def add_drink(self, input_drink):
         self.drinks.append(input_drink)
--- tests/Pub_test.py
+++ tests/Pub_test.py
@@ -1,16 +1,21 @@
 import unittest
 from src.pub import Pub
 from src.drink import Drink
+from src.customer import Customer
 
 class TestPub(unittest.TestCase):
 
     def setUp(self):
         self.Pub = Pub("King's Arms", 100)
         self.Pub.drinks = [Drink("Cider", 2), Drink("Beer", 5)]
+        self.Customer = Customer("John", 50, 25)
         
     def test_sell_drink(self):
-        self.Pub.sell_drink(self.Pub.drinks[0])
+        self.Pub.sell_drink(self.Pub.drinks[0], self.Customer.age)
         self.assertEqual(102, self.Pub.till)
+        sale_return = self.Pub.sell_drink(self.Pub.drinks[1], 15)
+        self.assertFalse(sale_return)
+        
 
     def test_increase_till(self):
         self.Pub.increase_till(5)
